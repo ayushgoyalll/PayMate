c1b72b192c57332415a8e23397398715b1169b26
ayushcodes5 <goyalayush3089@gmail.com>
2024-09-29 18:32:21 +0530

    added env file

--- backend/root/db.js
+++ backend/root/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect("");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL);
 const userSchema = new mongoose.Schema({
     username:{
         type: String,
